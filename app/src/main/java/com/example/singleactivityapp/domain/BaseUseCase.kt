667bdb4fc3e40aecf7a61acaf7ff5c3f3dec81d3
RamachandranKV <ramachandru6@gmail.com>
2026-01-10 23:08:24 +0530

    Formatted

--- app/src/main/java/com/example/singleactivityapp/domain/BaseUseCase.kt
+++ app/src/main/java/com/example/singleactivityapp/domain/BaseUseCase.kt
@@ -1,8 +1,5 @@
 package com.example.singleactivityapp.domain
 
-import com.example.singleactivityapp.data.newtwork.Users
-import kotlinx.coroutines.flow.Flow
-
 interface BaseUseCase<in P, out R> {
     suspend operator fun invoke(param: P): R
 }
